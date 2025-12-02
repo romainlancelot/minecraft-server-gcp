d592db09554863b4d8acba98abba658cd5f10fc4
Romain Lancelot <rlancelot@myges.fr>
2025-12-02 16:47:46 +0100

    Add main Terraform configuration for Google Compute Disk resource

---
+++ terraform/01_persistent/main.tf
@@ -0,0 +1,10 @@
+resource "google_compute_disk" "minecraft_data" {
+  name  = "minecraft-data"
+  type  = "pd-standard"
+  zone  = var.zone
+  size  = var.minecraft_disk_size
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
