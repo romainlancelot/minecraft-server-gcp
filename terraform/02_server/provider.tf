8cf11b988873592c1d0319e94a5f73714e0faf8f
Romain Lancelot <rlancelot@myges.fr>
2025-12-03 10:48:52 +0100

    Remove Terraform provider configuration file as part of project restructuring

--- terraform/02_server/provider.tf
+++ terraform/02_server/provider.tf
@@ -1,1 +1,18 @@
-../provider.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.12.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "tf-bucket-state"
+    prefix = "terraform/state/minecraft/server"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
